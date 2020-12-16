53f0a00803bb6f9cdf436d04f5fe1668c2f9a646
Tyler Weisner <weisnert@cse.msu.edu>
2020-12-16 15:41:49 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Tyler Weisner on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
